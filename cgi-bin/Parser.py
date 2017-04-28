68ebe5f31c9012990dd5956aef9025576f468b6c
kthom925 <kdt33@case.edu>
2017-04-28 10:05:03 -0400

    Finalized addition of the parser

--- cgi-bin/Parser.py
+++ cgi-bin/Parser.py
@@ -1,189 +1,209 @@
-import re
-import json
+#!C:\Python27\Python.exe
 import cgitb; cgitb.enable()
 import cgi
+import re
 
-class Parser:
+def __init__():
+    return
+dataFromJS = cgi.FieldStorage()
+print("Content-Type: text/html\n\n")
+codeLines = dataFromJS.list[0].value.split('\n')
+allVars = []
+startLine = -1
+stepCounter = 1
+steps = []
+lineMax = len(codeLines)-1
+outputString = '{"lines":['
 
-    mainDeclared = False
-    varType = []
-    varName = []
-    varValue = []
-    arrayIterator = 0
-    lineNumber = 0
-    outputFile = 'output.txt'
 
+def getNextLine(line, count):
+    global startLine
+    newVarName = ""
+    newVarVal = 0
+    if line.find("static void Main(") != -1:
+        startLine = count
+        return False
+    line = untab(line)
+    if line[:2] == "//": #if the line is a comment, do nothing
+        return False
+    if searcher(line, "int", "I", 0, count) or searcher(line, "string", "S", "null", count):
+        return True
+    
+def variableChangeSearch(lineNum):
+    global outputString
+    if lineNum >= lineMax or lineNum >=300:
+        outputString = outputString[:-1]
+        outputString += ']}'
+        print(outputString)
+        return
+    line = codeLines[lineNum]
+    line = untab(line)  
+    if line.find(';') == -1 or getNextLine(line, lineNum):
+        variableChangeSearch(lineNum+1)
+        return
+    firstWord = line[:line.find(' ')]
+    affectedVar = -1
+    counter = -1
+    for var in allVars:
+        counter += 1
+        if var[0][1:] == firstWord:
+            affectedVar = counter
+    firstSpace = line.find(' ')
+    operation = line[firstSpace+1]
+    if line[firstSpace + 2] in ['+','-','=']:
+        operation += line[firstSpace+2]
+    tempOp = line[firstSpace+3:-1].replace(" ","")
+    opVars = []
+    #Check if there are any operators on the RHS
+    otherOp = ''
+    if tempOp.find('+') != -1:
+        opVars = tempOp.split('+')
+        otherOp = '+'
+    elif tempOp.find('-') != -1:
+        opVars = tempOp.split('-')
+        otherOp = '-'
+    elif tempOp.find('*') != -1:
+        opVars = tempOp.split('*')
+        otherOp = '*'
+    elif tempOp.find('/') != -1:
+        opVars = tempOp.split('/')
+        otherOp = '/'
+    else:
+        #If not, affect the LHS by whatever operator is present
 
-    def __init__(self, outputFile='output.txt'):
-        if not(outputFile is None):
-            self.outputFile = outputFile
-
-    def getNextLine(line):
-
-        if mainDeclared == False: #is a main fxn declared?
-
-            mainDeclaration() #if not, look to see if this line declares it
-
+        #If no other value is present, just a simple increment or decrement
+        if operation == "++":
+            allVars[affectedVar][1] += 1
+        if operation == "--":
+            allVars[affectedVar][1] -= 1
+        #Check to see if the RHS is a variable
+        affectingVar = -1
+        counter = -1
+        for var in allVars:
+            counter += 1
+            if var[0][1:] == tempOp:
+                affectingVar = counter
+        #If the left hand side is only one value, adjust the value accordingly
+        if affectingVar == -1:
+            if operation == "=":
+                allVars[affectedVar][1] = tempOp
+            if operation == "+=":
+                allVars[affectedVar][1] += tempOp
+            if operation == "-=":
+                allVars[affectedVar][1] -= tempOp
+            if operation == "*=":
+                allVars[affectedVar][1] *= tempOp
+            if operation == "/=":
+                allVars[affectedVar][1] /= tempOp
         else:
+            if operation == "=":
+                allVars[affectedVar][1] = allVars[affectingVar][1]
+            if operation == "+=":
+                allVars[affectedVar][1] += allVars[affectingVar][1]
+            if operation == "-=":
+                allVars[affectedVar][1] -= allVars[affectingVar][1]
+            if operation == "*=":
+                allVars[affectedVar][1] *= allVars[affectingVar][1]
+            if operation == "/=":
+                allVars[affectedVar][1] /= allVars[affectingVar][1]
 
-            searcher(line) #if declared, look to see if anything valuable is on the line
-
-    def mainDeclaration(line):
-
-        if mainDeclared == False: #main fxn has not been called yet
-
-            if re.compile(r'''(
-                #(.*)
-                ()'static void main') ?
-                ''') is None:
-                #if main method not declared
-                    getNextLine(line)
-
-            else:
-            #main has been declared
-                mainDeclared = True
-                searcher(line)
-
-    def searcher(line):
-
-        if re.compile(r'''(
-             (byte|sbyte|int|uint|short|ushort|long|ulong|float|double|char|decimal) #finds type if mainDeclaration
-             )''') is None:
-             #nothing declared, search to see if a variable changes
-                 variableChangeSearch(line)
-
+    if len(opVars) == 2:
+        #Given that the RHS has 2 values, check if either, both, or neither of them are variables
+        affectingVar1 = -1
+        counter = -1
+        for var in allVars:
+            counter += 1
+            if var[0][1:] == opVars[0]:
+                affectingVar1 = counter
+        affectingVar2 = -1
+        counter = -1
+        for var in allVars:
+            counter += 1
+            if var[0][1:] == opVars[1]:
+                affectingVar2 = counter
+        lhs = 0
+        #if the first value isn't a variable, but just a literal
+        if affectingVar1 == -1:
+            lhs = opVars[0]
         else:
+            lhs = allVars[affectingVar1][1]
+        #If the second value isn't a variable
+        if affectingVar2 == -1:
+            if otherOp == "+":
+                lhs += opVars[1]
+            if otherOp == "-":
+                lhs -= opVars[1]
+            if otherOp == "*":
+                lhs *= opVars[1]
+            if otherOp == "/":
+                lhs /= opVars[1]
+        else:
+            if otherOp == "+":
+                lhs += allVars[affectingVar2][1]
+            if otherOp == "-":
+                lhs -= allVars[affectingVar2][1]
+            if otherOp == "*":
+                lhs *= allVars[affectingVar2][1]
+            if otherOp == "/":
+                lhs /= allVars[affectingVar2][1]
 
-            varType = group()
+        #add the operations completed on the RHS to the LHS with whatever base operation
+        if operation == "=":
+            allVars[affectedVar][1] = lhs
+        if operation == "+=":
+            allVars[affectedVar][1] += lhs
+        if operation == "-=":
+            allVars[affectedVar][1] -= lhs
+        if operation == "*=":
+            allVars[affectedVar][1] *= lhs
+        if operation == "/=":
+            allVars[affectedVar][1] /= lhs
+    makeJSON(lineNum)
+    variableChangeSearch(lineNum+1)
 
-            regexSearch = re.compile(r'''(
-                (\w+)   #find variable name
-            )''')
 
-            varName[arrayIterator] = group()
+def makeJSON(lineNum):
+    global outputString
+    global stepCounter
+    outputString += '{"'+str(stepCounter)+'": {"lineNumber": '+str(lineNum+1)+','
+    for var in allVars:
+        outputString += '"'+str(var[0])+'": '+str(var[1])+','
+    outputString = outputString[:-1]
+    outputString += '}},'
+    stepCounter += 1
+    return
 
-            if re.compile(r'''(
-                         (=)?    #does the variable have a value declared?
-                          )''') is None:
+def untab(line):
+    #adjust for tabbing so the start of the line is constant
+    while len(line) > 0 and line[0] == '\t' or line[0] == ' ':
+        line = line[1:]
+    return line
 
-                varValue[arrayIterator] = None
-                arrayIterator += 1
-
-                #makeJSON()
-
+def searcher(line, name, identifier, default, lineNum):
+    if line[:len(name)+1] == name+" " and line[len(line)-1] == ';':
+        newVarName = line[len(name)+1:line.find(" ",len(name)+1)]
+        eqInd = line.find("=")
+        if eqInd == -1: #the variable wasn't instantiated
+            newVarVal = 0
+        else:
+            if line[eqInd+1] == ' ':
+                newVarVal = line[eqInd+2:-1]
             else:
-
-                regexSearch = re.compile(r'''(
-                    (\w+)   #find the name
-                )''')
-
-                varValue[arrayIterator] = group()
-                arrayIterator += 1
-                #makeJSON()
-
-    def variableChangeSearch():
-
-        regexSearch = re.compile(r'''(
-                                (\w+)   #find the name; group 1
-                                (\s*)   #0 or more spaces; group 2
-                                (=|\+\+|--) #equals sign, plus plus, or minus minus; group 3
-                                (\s*)	#pasrse out the spaces; group 4
-                                (\w*)	#any variables used in equation; group 5
-                                 )''')
+                newVarVal = line[eqInd+1:-1]
+        allVars.append([identifier+newVarName, int(newVarVal)])
+        makeJSON(lineNum)
+        return True
+    return False
 
 
-        found = False
+startCount = 0
+for line in range(len(codeLines)):
+    getNextLine(codeLines[line], line)
+    if(startLine != -1):
+        break
+variableChangeSearch(startLine)
 
-        i = 0
-        while (i < arrayIterator):
 
-            if (varName[i] == group(1)):
+    
 
-                i = arrayIterator
-                found = True
 
-            else:
-
-                i += 1
-
-        if found and (group(5) is None):
-
-            if group(3) == '++':
-
-                if isinstance(varType[arrayIterator], types.StringTypes):
-
-                    varValue[arrayIterator] += 1
-
-        elif group(3) == '--':
-
-            if isinstance(varType[arrayIterator], types.StringTypes):
-
-                varValue[arrayIterator] -= 1
-
-        elif found:
-
-            regexSearch = re.compile(r'''(
-                                    ((\w*)
-                                      \s*
-                                    (\+|-)
-                                      \s*)*
-                                     )''')
-
-        operators = (len(group(0)) - 1) / 2 #number of operators
-        varsUsed = group(1)
-        opsUsed = group(2)
-
-        j = 0
-
-        while j < len(varsUsed):
-
-            if isinstance(varsUsed[j], types.StringTypes):
-
-                k = 0
-
-                while k < len(varName):
-
-                    if varName[k] == varsUsed[j]:
-
-                        varsUsed[j] = varValue[k]
-                    k += 1
-            j += 1
-
-        result = varsUsed[0]
-
-        j = 0
-
-        while j < len(opsUsed):
-
-            if opsUsed[j] == '+':
-
-                result = result + varsUsed[j + 1]
-
-            elif opsUsed[j] == '-':
-
-                result = result - varsUsed[j + 1]
-
-            j += 1
-
-        self.varValue[self.arrayIterator] = result
-
-    def makeJSON():
-
-        data = [lineNumber, varType, varName, varValue]
-        with open('output.txt', 'r+') as outputFile:
-            json.dump(data, outfile)
-        outputFile.close()
-
-        #takes lineNumber, varType array, varName array, and varValue array and puts into JSON format
-        #sends JSON to frontend
-
-    def main(self): #iterate through lines of textfile
-
-        p = Parser.parser()
-        p.lineNumber = 0
-        dataFromJS = cgi.FieldStorage()
-        codeLines = dataFromJS.list[0].value.split('\n')
-        while (len(codeLines) > 0 and (p.lineNumber <= 300)):
-            p.getNextLine(codeLines[0])
-            codeLines.pop(0)
-            p.lineNumber += 1
